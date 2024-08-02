c2bfee6ac8d165b533c77b1e43e8332bc442bd0e
RuslanTsitser <cicer_ruslan@mail.com>
2024-08-02 19:18:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# multimedia_example
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/tsitser/dev/multimedia_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tsitser.dev.multimedia_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
